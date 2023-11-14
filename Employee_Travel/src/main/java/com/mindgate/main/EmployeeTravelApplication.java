1f3b767abc6f9e845aba5106fb09145ef89e0a74
MGSCHN-SIBI RG <MGSCHN-SIBI RG@MGSCHN-LAP-341>
2023-11-14 17:46:20 +0530

    agents

--- Employee_Travel/src/main/java/com/mindgate/main/EmployeeTravelApplication.java
+++ Employee_Travel/src/main/java/com/mindgate/main/EmployeeTravelApplication.java
@@ -10,14 +10,7 @@ public class EmployeeTravelApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeTravelApplication.class, args);
 		System.out.println("Employee Travel Is Deployed Successfully!");
-		System.out.println("This is new change");
-		System.out.println("This line is added by sibi");
-		System.out.println("this line is added by priya");
-		
-		System.out.println("this is team lead");
-		System.out.println("spring boot ");
-		System.out.println("By Maggie");
-		System.out.println("OKay NOw no issue with class files or properties");
+		System.out.println("Agent Updated by Sibi-14/11/23 5.45pm");
 	}
 
 }
