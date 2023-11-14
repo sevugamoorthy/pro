98ecb144bae66b282d5aba80b61549635892220f
sevugamoorthy <sevugam60@gmail.com>
2023-11-14 18:16:24 +0530

    hi

--- Employee_Travel/src/main/java/com/mindgate/main/EmployeeTravelApplication.java
+++ Employee_Travel/src/main/java/com/mindgate/main/EmployeeTravelApplication.java
@@ -12,6 +12,7 @@ public class EmployeeTravelApplication {
 		System.out.println("Employee Travel Is Deployed Successfully!");
 		System.out.println("Agent Updated by Sibi-14/11/23 5.59 pm");
 		System.out.println("@@@@@@@@@@@@");
+		System.out.println("hi");
 	}
 
 }
